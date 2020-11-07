aa8db36046066fbf9d02131da650981ad224dd2e
sakiponjuice <saki.pon.juice0922@gmail.com>
2020-11-07 02:38:10 +0000

    [Finish] 3-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,12 +6,18 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
   end
   
   def create
-  
+    blog = Blog.new(blog_params)
   end
 
   def edit
+  end
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
   end
 end
